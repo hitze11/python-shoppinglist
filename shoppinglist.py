3e8f020182eb4dedf2c6959e19553d38e689a024
hitze <hitze1126@googlemail.com>
2024-10-02 13:54:07 +0200

    Einkaufsliste erstellt

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,1 +1,15 @@
-shoppingliste
+# shoppinglist.py
+
+# Leere Einkaufsliste
+shoppinglist = []
+
+# Funktion zum Hinzufügen eines Artikels
+def add_item():
+    item = input("Welche Artikel sollen auf die Einkaufsliste hinzugefügt werden: ")
+    shoppinglist.append(item)
+    print(f"{item} wurde der Einkaufsliste hinzugefügt.")
+
+# Einkaufsliste mit Arikeln
+if __name__ == "__main__":
+    add_item()
+    print("Aktuelle Einkaufsliste:", shoppinglist)
