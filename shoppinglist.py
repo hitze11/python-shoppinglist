75b878da1de3d24e74b9d5a93e7172fd6dbf2c14
hitze <hitze1126@googlemail.com>
2024-10-10 13:03:36 +0200

    update

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,44 +1,49 @@
-# shoppinglist.py
-
-# Leere Einkaufsliste
+# Dies ist ein Programm das als Shopping Liste funktionieren soll
+ 
+# Erstellung der Liste
 shoppinglist = []
 
-#  Hinzufügen eines Artikels
+# Funktion um ein Item der Liste hinzuzufügen
 def add_item():
-    item = input("Welche Artikel sollen dem Einkaufszettel hinzugefügt werden?: ")
-    shoppinglist.append(item)
-    print(f"{item} wurde dem Eikaufszettel hinzugefügt.")
-
-# Anzeigen der Einkaufsliste
-def show_shoppinglist():
-    if shoppinglist:
-        print("Einkaufszettel:")
-        for item in shoppinglist:
-            print(f"- {item}")  
+    item = input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll: ")
+    if item:
+        shoppinglist.append(item)
+        print(f"Der Artikel {item} wurde der Einkaufsliste hinzugefügt")
     else:
-        print("Dein Einkaufszettel ist leer.")
+        print("Eingabe war leer und daher wird kein Artikelt hinzugefügt.")
 
-# Hauptfunktion, um das Programm zu steuern
+# Aufruf der Funktion add_item
+# add_item(input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll: "))
+
+# Funktion um die EInkaufsliste anzuzeigen
+def show_shoppinglist():
+    print("Deine Einkaufsliste:")
+    if len(shoppinglist) != 0:
+        for i in (shoppinglist):
+            print(i)
+    else:
+        print("Deine Einkaufliste ist leer")
+
+# Aufruf der Funktion show_shoppinglist
+# show_shoppinglist()
+
+# Main Function
 def main():
-    while True:
-        print("\n----- Einkaufszettel -----")
-        print("1. Artikel zum Einkaufszettel hinzufügen")
-        print("2. Einkaufszettel anzeigen")
+    while 1 == 1:
+        print("\n----- Einkaufsliste -----")
+        print("1. Artikel zur Einkaufsliste hinzufügen")
+        print("2. Einkaufsliste anzeigen")
         print("3. Programm beenden")
-        
-        # Benutzerauswahl
-        choice = input("Bitte wähle eine Option (1, 2 oder 3): ")
-        
+        choice = input("Bitte wähle aus was du machen möchtest: ")
         if choice == "1":
             add_item()
         elif choice == "2":
             show_shoppinglist()
         elif choice == "3":
             print("Programm wird beendet! Auf Wiedersehen.")
-            break  
+            break
         else:
-            print("Ungültige Auswahl. Bitte wähle 1, 2 oder 3.")
+            print("Ungültige Auswahl. Bitte wähle 1, 2 oder 3")
 
-# Einkaufszettel anzeigen
 if __name__ == "__main__":
     main()
